5a8459d3535c2818655a182b1f69909a36662a54
전하은 <hani@Hanis-Mac-Book.local>
2023-06-14 15:27:44 +0900

    add proxy file

---
+++ src/setProxy.js
@@ -0,0 +1,10 @@
+import BASE_URL from "./config";
+const { proxy } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    proxy("/", {
+      target: BASE_URL,
+    })
+  );
+};
